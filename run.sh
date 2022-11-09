3f7b20152c3f083e85a09615195f60a88c328121
Yahya Jabary <jabaryyahya@gmail.com>
2022-11-09 11:41:52 +0100

    lol i have a memory leak for some reason wtf

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 make ispalindrom
-printf "−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−\n"
+printf "−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−\n"
 
 ./ispalindrom -s -i --outfile ./output.txt ./input1.txt ./input2.txt
 
-printf "−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−\n"
+printf "−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−\n"
 make clean
-
-# valgrind --leak-check=full --show-leak-kinds=all -s ./ispalindrom -s -i --outfile ./output.txt ./input1.txt ./input2.txt
---
+++ valgrind.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+make ispalindrom
+
+valgrind --track-origins=yes --leak-check=full --show-leak-kinds=all -s \
+./ispalindrom \
+-s -i --outfile ./output.txt ./input1.txt ./input2.txt
