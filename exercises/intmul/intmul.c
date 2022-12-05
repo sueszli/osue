3c07c1653dfc10c85d743a73611e9e92d27fda64
Yahya Jabary <jabaryyahya@gmail.com>
2022-12-05 18:06:39 +0100

    wrote addLeadingZeroes function -> next integrate into code

--- exercises/intmul/intmul.c
+++ exercises/intmul/intmul.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -12,26 +13,114 @@
 
 #define usage(msg)                                                          \
   do {                                                                      \
-    fprintf(                                                                \
-        stderr,                                                             \
-        "Invalid input: %s.\nEnter 2 hexadecimal numbers to multiply with " \
-        "eachother.\nSYNOPSIS:\n\t./intmul {hexNum1} {hexNum2}",            \
-        msg);                                                               \
+    fprintf(stderr,                                                         \
+            "Invalid input: %s\nCalculates the product of 2 hex numbers.\n" \
+            "SYNOPSIS: ./intmul\n",                                         \
+            msg);                                                           \
     exit(EXIT_FAILURE);                                                     \
   } while (true);
 
-void validateInput(int argc, char* argv[]) {
-  if (argc < 3) {
-    usage("too few arguments");
+typedef struct {
+  char* hex1;
+  char* hex2;
+  size_t len;  // same for both
+} HexStringPair;
+
+static char* addLeadingZeros(char* str, size_t diff) {
+  // pre-condition: str must be dynamically allocated
+  // post-condition: output must be freed (str must not be freed)
+
+  size_t oldSize = strlen(str) + 1;
+  size_t newSize = oldSize + diff;
+
+  char* oldStrCopy = strdup(str);
+  if (oldStrCopy == NULL) {
+    error("strdup");
+  }
+  char* newStr = realloc(str, newSize * sizeof(*str));
+  if (newStr == NULL) {
+    error("realloc");
   }
 
-  char* hexNum1 = argv[1];
-  char* hexNum2 = argv[2];
-  printf("%s\n%s\n", hexNum1, hexNum2);
+  for (size_t i = 0; i < oldSize; i++) {
+    newStr[i] = '0';
+    newStr[i + diff] = oldStrCopy[i];
+  }
+
+  free(oldStrCopy);
+  return newStr;
+}
+
+static void validateInput(HexStringPair pair) {
+  // side-effect: adds leading zeros such that pair has same length
+
+  const char* valid = "0123456789abcdefABCDEF";
+  if ((strspn(pair.hex1, valid) != strlen(pair.hex1)) ||
+      (strspn(pair.hex2, valid) != strlen(pair.hex2))) {
+    free(pair.hex1);
+    free(pair.hex2);
+    usage("argument is not a hexadecimal number")
+  }
+
+  if (strlen(pair.hex1) < strlen(pair.hex2)) {
+    size_t diff = strlen(pair.hex2) - strlen(pair.hex1);
+    addLeadingZeros(pair.hex1, diff);
+
+  } else if (strlen(pair.hex1) > strlen(pair.hex2)) {
+    size_t diff = strlen(pair.hex1) - strlen(pair.hex2);
+    addLeadingZeros(pair.hex2, diff);
+  }
+
+  // add leading zeros if their size is not 2^n
+
+  pair.len = strlen(pair.hex1);
+}
+
+static HexStringPair getInput(void) {
+  // post-condition: free returned HexStringPair
+
+  HexStringPair hexStringPair;
+
+  char* line = NULL;
+  size_t len = 0;
+  ssize_t nChars;
+  int nLines = 0;
+  while ((nChars = getline(&line, &len, stdin)) != -1) {
+    if (nLines == 0) {
+      line[strlen(line) - 1] = '\0';
+      hexStringPair.hex1 = strdup(line);
+      nLines++;
+    } else if (nLines == 1) {
+      line[strlen(line) - 1] = '\0';
+      hexStringPair.hex2 = strdup(line);
+      nLines++;
+    } else {
+      break;
+    }
+  }
+  free(line);
+
+  if (nLines == 1) {
+    free(hexStringPair.hex1);
+    usage("only 1 argument");
+  }
+
+  // validateInput(hexStringPair);
+  return hexStringPair;
 }
 
 int main(int argc, char* argv[]) {
-  validateInput(argc, argv);
+  if (argc > 1) {
+    usage("no arguments allowed");
+  }
+  HexStringPair hexStringPair = getInput();
+
+  printf("hex1: %s\n", hexStringPair.hex1);
+  printf("hex2: %s\n", hexStringPair.hex2);
+  // printf("len: %d\n", hexStringPair.len);
+
+  free(hexStringPair.hex1);
+  free(hexStringPair.hex2);
 
   return EXIT_SUCCESS;
 }
--- exercises/intmul/test.sh
+++ exercises/intmul/test.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
+ARGS="3\nAB3"
+
 # examples
-#ARGS="3 3"
-#ARGS="1A B3"
-#ARGS="1000 0001"
-ARGS="13A5D87E85412E5F 7812C53B014D5DF8"
+#ARGS="3\n3"
+#ARGS="1A\nB3"
+#ARGS="1000\n0001"
+#ARGS="13A5D87E85412E5F\n7812C53B014D5DF8"
 
 # mandatory test cases: valid inputs
-#ARGS="3 4"
-#ARGS="3 3"
-#ARGS="1A b3"
-#ARGS="1000 0001"
-#ARGS="aB 7"
-#ARGS="Deadbe ef"
-#ARGS="13A5D87E85412E5F 7812C53B014D5DF8"
-#ARGS="fe6975a937016c20b43b17540e6c6246cc5e9216b37b3f82dfe398ed311d16e63a0666c07bad1a857d8359a70dedb685 dbd432ede8ea89ff7a24d6d7b47513c5c9dc7db2c2a84ccf1176c37e292185f7b79dafd9f1b32a27598872bba8a8f5da"
+#ARGS="3\n4"
+#ARGS="3\n3"
+#ARGS="1A\nb3"
+#ARGS="1000\n0001"
+#ARGS="aB\n7"
+#ARGS="Deadbe\nef"
+#ARGS="13A5D87E85412E5F\n7812C53B014D5DF8"
+#ARGS="fe6975a937016c20b43b17540e6c6246cc5e9216b37b3f82dfe398ed311d16e63a0666c07bad1a857d8359a70dedb685\ndbd432ede8ea89ff7a24d6d7b47513c5c9dc7db2c2a84ccf1176c37e292185f7b79dafd9f1b32a27598872bba8a8f5da"
 
 # mandatory test cases: invalid inputs
 #ARGS="1"
-#ARGS="1 x"
-#ARGS="2 2.0"
+#ARGS="1\nx"
+#ARGS="2\n2.0"
 
 clear
 
@@ -27,7 +29,7 @@ echo -e "--------- COMPILING ---------"
 make all
 
 echo -e "\n\n--------- RUNNING WITH VALGRIND ---------"
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s ./intmul $ARGS
+echo -e $ARGS | valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s ./intmul
 
 echo -e "\n\n--------- CLEANING ---------"
 make clean
