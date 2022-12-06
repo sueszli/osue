1f412d9f5e422d255c5f48cbb0836bfb3463a526
Yahya Jabary <jabaryyahya@gmail.com>
2022-12-06 03:30:44 +0100

    refactored and everything is still fully functional

--- exercises/intmul/intmul.c
+++ exercises/intmul/intmul.c
@@ -24,16 +24,16 @@
   } while (true);
 
 typedef struct {
-  char* hex1;
-  char* hex2;
+  char* a;
+  char* b;
   size_t len;
 } HexStringPair;
 
 typedef struct {
-  char* hex1H;
-  char* hex1L;
-  char* hex2H;
-  char* hex2L;
+  char* aH;
+  char* aL;
+  char* bH;
+  char* bL;
   size_t len;
 } HexStringQuad;
 
@@ -76,10 +76,10 @@ static HexStringPair getInput(void) {
   while ((nChars = getline(&line, &lineLen, stdin)) != -1) {
     if (nLines == 0) {
       line[strlen(line) - 1] = '\0';
-      pair.hex1 = strdup(line);
+      pair.a = strdup(line);
     } else if (nLines == 1) {
       line[strlen(line) - 1] = '\0';
-      pair.hex2 = strdup(line);
+      pair.b = strdup(line);
     }
     nLines++;
   }
@@ -87,47 +87,47 @@ static HexStringPair getInput(void) {
 
   // validate input
   if (nLines == 1) {
-    free(pair.hex1);
+    free(pair.a);
     usage("too few argument");
   }
   if (nLines > 2) {
-    free(pair.hex1);
-    free(pair.hex2);
+    free(pair.a);
+    free(pair.b);
     usage("too many arguments");
   }
-  if ((strlen(pair.hex1) == 0) || (strlen(pair.hex2) == 0)) {
-    free(pair.hex1);
-    free(pair.hex2);
+  if ((strlen(pair.a) == 0) || (strlen(pair.b) == 0)) {
+    free(pair.a);
+    free(pair.b);
     usage("empty argument");
   }
   const char* valid = "0123456789abcdefABCDEF";
-  if ((strspn(pair.hex1, valid) != strlen(pair.hex1)) ||
-      (strspn(pair.hex2, valid) != strlen(pair.hex2))) {
-    free(pair.hex1);
-    free(pair.hex2);
+  if ((strspn(pair.a, valid) != strlen(pair.a)) ||
+      (strspn(pair.b, valid) != strlen(pair.b))) {
+    free(pair.a);
+    free(pair.b);
     usage("one argument is not a hexadecimal number")
   }
 
   // get len to the same in pair
-  const size_t len1 = strlen(pair.hex1);
-  const size_t len2 = strlen(pair.hex2);
+  const size_t len1 = strlen(pair.a);
+  const size_t len2 = strlen(pair.b);
   const size_t diff = (size_t)labs((long)(len2 - len1));
   if (len1 < len2) {
-    addZeroes(&(pair.hex1), diff, true);
+    addZeroes(&(pair.a), diff, true);
   } else if (len1 > len2) {
-    addZeroes(&(pair.hex2), diff, true);
+    addZeroes(&(pair.b), diff, true);
   }
-  assert(strlen(pair.hex1) == strlen(pair.hex2));
+  assert(strlen(pair.a) == strlen(pair.b));
 
   // get len to be a power of 2
-  const size_t len = strlen(pair.hex1);
+  const size_t len = strlen(pair.a);
   if ((len & (len - 1)) != 0) {
     const size_t newLen = 1 << (size_t)ceill(log2l((long double)len));
-    addZeroes(&(pair.hex1), newLen - len, true);
-    addZeroes(&(pair.hex2), newLen - len, true);
+    addZeroes(&(pair.a), newLen - len, true);
+    addZeroes(&(pair.b), newLen - len, true);
   }
 
-  pair.len = strlen(pair.hex1);
+  pair.len = strlen(pair.a);
   return pair;
 }
 
@@ -148,18 +148,18 @@ static HexStringQuad getHexStringQuad(HexStringPair pair) {
   size_t size = pair.len / 2 + 1;
 
   // high digits
-  quad.hex1H = malloc(size * sizeof(char));
-  quad.hex2H = malloc(size * sizeof(char));
-  memcpy(quad.hex1H, pair.hex1, size - 1);
-  memcpy(quad.hex2H, pair.hex2, size - 1);
-  quad.hex1H[size - 1] = '\0';
-  quad.hex2H[size - 1] = '\0';
+  quad.aH = malloc(size * sizeof(char));
+  quad.bH = malloc(size * sizeof(char));
+  memcpy(quad.aH, pair.a, size - 1);
+  memcpy(quad.bH, pair.b, size - 1);
+  quad.aH[size - 1] = '\0';
+  quad.bH[size - 1] = '\0';
 
   // low digits
-  quad.hex1L = malloc(size * sizeof(char));
-  quad.hex2L = malloc(size * sizeof(char));
-  memcpy(quad.hex1L, (pair.hex1 + size - 1), size);
-  memcpy(quad.hex2L, (pair.hex2 + size - 1), size);
+  quad.aL = malloc(size * sizeof(char));
+  quad.bL = malloc(size * sizeof(char));
+  memcpy(quad.aL, (pair.a + size - 1), size);
+  memcpy(quad.bL, (pair.b + size - 1), size);
 
   quad.len = size - 1;
   return quad;
@@ -172,18 +172,18 @@ int main(int argc, char* argv[]) {
 
   // read input
   HexStringPair pair = getInput();
-  printf("hex1 pair: %s\n", pair.hex1);
-  printf("hex2 pair: %s\n", pair.hex2);
+  printf("a pair: %s\n", pair.a);
+  printf("b pair: %s\n", pair.b);
   printf("len: %ld\n\n", pair.len);
 
   // base case
   if (pair.len == 1) {
-    unsigned long long result = parseHexStr(pair.hex1) * parseHexStr(pair.hex2);
+    unsigned long long result = parseHexStr(pair.a) * parseHexStr(pair.b);
     fprintf(stdout, "RESULT: %llx\n", result);
     fflush(stdout);
 
-    free(pair.hex1);
-    free(pair.hex2);
+    free(pair.a);
+    free(pair.b);
     return EXIT_SUCCESS;
   }
 
@@ -196,8 +196,8 @@ int main(int argc, char* argv[]) {
       + aL * bL              -> sent to LL child
   */
   HexStringQuad quad = getHexStringQuad(pair);
-  printf("hex1 quad: %s %s\n", quad.hex1H, quad.hex1L);
-  printf("hex2 quad: %s %s\n", quad.hex2H, quad.hex2L);
+  printf("a pair: %s %s\n", quad.aH, quad.aL);
+  printf("b pair: %s %s\n", quad.bH, quad.bL);
   printf("len: %ld\n", quad.len);
 
   // open 2 pipes per child
@@ -224,13 +224,13 @@ int main(int argc, char* argv[]) {
   const int READ_END = 0;
   const int WRITE_END = 1;
 
-  free(quad.hex1H);
-  free(quad.hex1L);
-  free(quad.hex2H);
-  free(quad.hex2L);
+  free(quad.aH);
+  free(quad.aL);
+  free(quad.bH);
+  free(quad.bL);
 
-  free(pair.hex1);
-  free(pair.hex2);
+  free(pair.a);
+  free(pair.b);
 
   return EXIT_SUCCESS;
 }
