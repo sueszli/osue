b122611e7d5494d593edffcfd978b9ea44440f96
Yahya Jabary <jabaryyahya@gmail.com>
2022-12-10 14:53:45 +0100

    works

--- old exams/getopt exercise pool/my solutions/test.sh
+++ old exams/getopt exercise pool/my solutions/test.sh
@@ -1,22 +1,15 @@
 #!/bin/sh
 
-# exercise 0:
-ARGS=""
-ELF="./exercise0"
+ELF="./solution0" # <-- change this to your executable
+ARGS="" # <-- change this to your arguments
 
-# exercise 1:
+# --------------------------------------------
 
-# exercise 2:
+clear
 
-# exercise 3:
+FLAGS="-std=c99 -pedantic -Wall -Wextra -Wwrite-strings -Wconversion -g3 -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_SVID_SOURCE -D_POSIX_C_SOURCE=200809L"
+gcc -o $ELF $FLAGS $ELF.c 
 
-
-echo -e "--------- COMPILING ---------"
-FLAGS = "-std=c99 -pedantic -Wall -Wextra -Wwrite-strings -Wconversion -g3 -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_SVID_SOURCE -D_POSIX_C_SOURCE=200809L"
-gcc -o $FLAGS 
-
-echo -e "\n\n--------- RUNNING WITH VALGRIND ---------"
 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s $ELF $ARGS
 
-echo -e "\n\n--------- CLEANING ---------"
-rm -rf ...
+rm -rf $ELF
