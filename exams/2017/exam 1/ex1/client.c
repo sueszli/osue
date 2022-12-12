94f4c84527770a28cf479cd2e3254e01ddaf399b
Yahya Jabary <jabaryyahya@gmail.com>
2022-12-12 16:46:24 +0100

    update

--- exams/2017/exam 1/ex1/client.c
+++ exams/2017/exam 1/ex1/client.c
@@ -15,55 +15,33 @@ char *program_name = "<not yet set>";
 
 int main(int argc, char **argv)
 {
-    struct args arguments =
-        { DEFAULT_PORTNUM, DEFAULT_PORTNUM_STR, UNDEF, 0, 0 };
+	struct args arguments =
+	{ DEFAULT_PORTNUM, DEFAULT_PORTNUM_STR, UNDEF, 0, 0 };
 
-    /* set program_name */
-    if (argc > 0) {
-        program_name = argv[0];
-    }
+	/* set program_name */
+	if (argc > 0) {
+		program_name = argv[0];
+	}
 
-    /***********************************************************************
-     * Task 1
-     * ------
-     * Implement argument parsing for the client. Synopsis:
-     *   ./client [-p PORT] {-g|-s VALUE} ID
-     *
-     * Call usage() if invalid options or arguments are given (there is no
-     * need to print a description of the problem).
-     *
-     * Hints: getopt(3), UINT16_MAX, parse_number (client.h),
-     *        struct args (client.h)
-     ***********************************************************************/
+	/***********************************************************************
+	 * Task 1
+	 * ------
+	 * Implement argument parsing for the client. Synopsis:
+	 *   ./client [-p PORT] {-g|-s VALUE} ID
+	 *
+	 * Call usage() if invalid options or arguments are given (there is no
+	 * need to print a description of the problem).
+	 *
+	 * Hints: getopt(3), UINT16_MAX, parse_number (client.h),
+	 *        struct args (client.h)
+	 ***********************************************************************/
 
-    /* COMPLETE AND EXTEND THE FOLLOWING CODE */
-    int c;
-    int opt_p = 0;
-    long num;
+	/* PARSING WITH GETOPT (had to be implemented almost from scratch) */
 
-    while ((c = getopt(argc, argv, "p:")) != -1) {
-        switch (c) {
-        case 'p':
-            if (opt_p != 0)
-                usage();
+	/* DO NOT FORGET ABOUT THE POSITIONAL ARGUMENTS */
 
-            num = parse_number(optarg);
-            if (num < 1024 || num > UINT16_MAX)
-                usage();
+	/* DO NOT REMOVE THE FOLLOWING LINE */
+	apply_command(arguments);
 
-            arguments.portnum = (uint16_t) num;
-            arguments.portstr = optarg;
-            opt_p = 1;
-            break;
-
-        }
-    }
-
-    /* DO NOT FORGET ABOUT THE POSITIONAL ARGUMENTS */
-
-
-    /* DO NOT REMOVE THE FOLLOWING LINE */
-    apply_command(arguments);
-
-    return 0;
+	return 0;
 }
