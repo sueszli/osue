f5707b0b118cbfb82830d3fb48c107d1fa479e19
Yahya JABARY (OSUE participant) <y11912007@apps1.inflab.tuwien.ac.at>
2022-12-13 19:17:01 +0100

    solved ex1

--- exams/2017/exam 2/ex1/solution/validate.c
+++ exams/2017/exam 2/ex1/solution/validate.c
@@ -1,5 +1,6 @@
 #include "validate.h"
 
+#include <assert.h>
 #include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -76,8 +77,85 @@ int main(int argc, char *argv[]) {
  * @param expr The expression for `./calc`.
  */
 void task_1(char *iban, char expr[MAX_TEXTLEN]) {
-  /* REPLACE FOLLOWING LINE WITH YOUR SOLUTION */
-  // task_1_DEMO(iban, expr);
+  printf("input: %s\n", iban);
+  if (strlen(iban) > 999) {  // <--- test to get min size that is legal works
+    usage();
+  }
+
+  // place first 4 chars at the end
+  char four[5];
+  for (int i = 0; i < 4; i++) {  // <--- simplify these parts to memncpy
+    four[i] = iban[i];
+  }
+  four[4] = '\0';
+  printf("first four chars: %s\n", four);
+
+  // copy remaining chars
+  const size_t remainingLen = strlen(iban) - 4;
+  char *remaining = malloc((remainingLen + 1) * sizeof(char));
+  if (remaining == NULL) {
+    error_exit("malloc");
+  }
+  size_t rCounter = 0;
+  for (size_t i = 4; i <= strlen(iban); i++) {
+    remaining[rCounter++] = iban[i];
+  }
+  printf("remaining chars: %s\n", remaining);
+
+  // write into tmp in flipped order
+  char *tmp = malloc((strlen(iban) + 1) * sizeof(char));
+  if (tmp == NULL) {
+    error_exit("malloc");
+  }
+  size_t tCounter = 0;
+  for (size_t i = 0; i < strlen(remaining); i++) {
+    tmp[tCounter++] = remaining[i];
+  }
+  for (size_t i = 0; i < 4; i++) {
+    tmp[tCounter++] = four[i];
+  }
+  tmp[tCounter] = '\0';
+  free(remaining);
+  printf("tmp: %s\n", tmp);
+
+  // convert chars to digits, write into expr
+  size_t eCounter = 0;
+  for (size_t i = 0; i < strlen(tmp); i++) {
+    if (isdigit(tmp[i])) {
+      printf("already digit: %c\n", tmp[i]);
+      expr[eCounter++] = tmp[i];
+
+    } else if (isalpha(tmp[i])) {
+      printf("not digit: %c\n", tmp[i]);
+      int val = tmp[i] - 55;
+
+      int fstInt = val / 10;
+      assert(fstInt < 10);
+      char fstChar[2] = {'\0', '\0'};
+      sprintf(fstChar, "%d", fstInt);
+      printf("fst: %c\n", fstChar[0]);
+      expr[eCounter++] = fstChar[0];
+
+      int sndInt = val % 10;
+      assert(sndInt < 10);
+      char sndChar[2] = {'\0', '\0'};
+      sprintf(sndChar, "%d", sndInt);
+      printf("snd: %c\n", sndChar[0]);
+      expr[eCounter++] = sndChar[0];
+
+    } else {
+      usage();
+    }
+  }
+  free(tmp);
+  expr[eCounter++] = '%';
+  expr[eCounter++] = ' ';
+  expr[eCounter++] = '9';
+  expr[eCounter++] = '7';
+  expr[eCounter] = '\0';
+  printf("expr: %s\n", expr);
+
+  task_1_DEMO(iban, expr);
 }
 
 /***************************************************************************
@@ -101,7 +179,7 @@ void task_1(char *iban, char expr[MAX_TEXTLEN]) {
  */
 void task_2(int fd[2], char expr[MAX_TEXTLEN], char result[MAX_TEXTLEN]) {
   /* REPLACE FOLLOWING LINE WITH YOUR SOLUTION */
-  // task_2_DEMO(fd, expr, result);
+  task_2_DEMO(fd, expr, result);
 }
 
 /****************************************************************************
@@ -125,5 +203,5 @@ void task_2(int fd[2], char expr[MAX_TEXTLEN], char result[MAX_TEXTLEN]) {
  */
 void task_3(int fd[2], char expr[MAX_TEXTLEN]) {
   /* REPLACE FOLLOWING LINE WITH YOUR SOLUTION */
-  // task_3_DEMO(fd, expr);
+  task_3_DEMO(fd, expr);
 }
