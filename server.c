1e4791b6c4211f091cceacf2487acc70dc3d49ba
Yahya Jabary <jabaryyahya@gmail.com>
2023-01-09 20:28:15 +0100

    update

--- server.c
+++ server.c
@@ -1,11 +1,13 @@
 #include <errno.h>
 #include <netdb.h>
+#include <signal.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -25,6 +27,25 @@
     perror(msg);        \
     exit(EXIT_FAILURE); \
   } while (0);
+
+static volatile sig_atomic_t quit = false;
+static void onSignal(int sig, siginfo_t* si, void* unused) { quit = true; }
+static void initSignalListener(void) {
+  struct sigaction sa;
+  sa.sa_flags = SA_SIGINFO;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_sigaction = onSignal;
+
+  if (sigaction(SIGINT, &sa, NULL) == -1) {
+    error("sigaction");
+  }
+  if (sigaction(SIGTERM, &sa, NULL) == -1) {
+    error("sigaction");
+  }
+  if (sigaction(SIGQUIT, &sa, NULL) == -1) {
+    error("sigaction");
+  }
+}
 
 typedef struct {
   char* port;
@@ -57,7 +78,7 @@ static void validateArguments(Arguments args) {
     }
   }
 
-  const char* illegalPathChars = "\\:*?\"<>|";  // unix is less strict
+  const char* illegalPathChars = "\\:*?\"<>|";
   if (args.rootPath != NULL) {
     if (strspn(args.rootPath, illegalPathChars) != 0) {
       usage("root path name contains illegal characters");
@@ -118,6 +139,62 @@ static Arguments parseArguments(int argc, char* argv[]) {
 
 int main(int argc, char* argv[]) {
   Arguments args = parseArguments(argc, argv);
+
+  initSignalListener();
+
+  // get socket list and store in &result
+  struct addrinfo* result;
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+  int s;
+  if ((getaddrinfo(NULL, args.port, &hints, &result)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+    error("getaddrinfo");
+  }
+
+  // iterate through results until a connection succeeds
+  int sockfd;
+  struct addrinfo* rp;
+  for (rp = result; rp != NULL; rp = rp->ai_next) {
+    sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+    if (sockfd == -1) {
+      continue;
+    }
+    if (bind(sockfd, rp->ai_addr, rp->ai_addrlen) == 0) {
+      break;  // success
+    }
+    close(sockfd);
+  }
+  freeaddrinfo(result);
+  if (rp == NULL) {
+    error("no connection could be established");
+  }
+
+  // set port to be immediately reusable
+  const int optval = 1;
+  setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
+
+  // open socket
+  const int queueLen = 1 << 4;
+  if (listen(sockfd, queueLen) == -1) {
+    error("listen");
+  }
+
+  // listen to requests
+  while (!quit) {
+    int reqfd = accept(sockfd, NULL, NULL);
+    if (reqfd == -1) {
+      error("accept");
+    }
+
+    FILE* socketStream = fdopen(reqfd, "w+");
+    if (socketStream == NULL) {
+      error("fdopen");
+    }
+  }
 
   exit(EXIT_SUCCESS);
 }
