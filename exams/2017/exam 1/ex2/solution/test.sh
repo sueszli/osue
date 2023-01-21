46ebd6b666e64ffa51cae6691d9a25ad1e70a5e0
Yahya JABARY (OSUE participant) <y11912007@apps1.inflab.tuwien.ac.at>
2023-01-21 14:15:56 +0100

    update

--- exams/2017/exam 1/ex2/solution/test.sh
+++ exams/2017/exam 1/ex2/solution/test.sh
@@ -6,15 +6,8 @@ compile and run server:
   make all && valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./server
 
 run client based on `./client [-p PORT] {-g|-s VALUE} ID`
-  ./client -s 50 24
-  ./client -g 31
-
-
-# -------
-
-clear
-
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./client $ARGS
+  valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./client -s 50 24
+  valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./client -g 31
 
 make clean
 
